6f6c2cae3b8c788850a9f9e70ed9de85fe2438bc
Filip Sunnemar <filipsunnemar@hotmail.com>
2020-08-26 17:06:40 +0200

    import arklogo

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './NavbarStyles.css'
+import Logo from '../../Pictures/Ark_logo.png'
 
 const Navbar = () => {
     return (
         <>
             <div className="Navbar">
+                <img className="Logo" src={Logo} alt="Ark Logo" />
 
             </div>
         </>
