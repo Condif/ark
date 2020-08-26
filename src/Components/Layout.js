84e54d74ef85197e59072369f36265afeeb3ae54
Filip Sunnemar <filipsunnemar@hotmail.com>
2020-08-26 15:15:41 +0200

    File setup

--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -1,5 +1,21 @@
+import React from "react";
+import Navbar from "./Navbar/Navbar";
+import Header from "./Header/Header";
+import ProductGrid from "./ProductGrid/ProductGrid";
+import Contact from "./Contact/Contact";
+import Footer from "./Footer/Footer";
+
+
 const Layout = () => {
-    return null
+    return (
+        <>
+            <Navbar/>
+            <Header/>
+            <ProductGrid/>
+            <Contact/>
+            <Footer/>
+        </> 
+    )
 }
 
 export default Layout;
