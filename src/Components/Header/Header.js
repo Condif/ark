ce156a6e3a51c0c3909a7504a37862f6c8563984
Filip Sunnemar <filipsunnemar@hotmail.com>
2020-08-27 15:42:43 +0200

    header pos

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import './HeaderStyles.css'
-import Lego_bg from '../../Pictures/Lego_bg.png'
 
 const Header = () => {
     return (
         <>
             <div className="Header">
-                 <img className="Lego_bg" src={Lego_bg} alt="Lego background" />
+                <div className="Lego_bg"></div>
             </div>
         </>
     )
