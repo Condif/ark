4bef31629c7d1a2c871d95e4622ac86083b19a96
Filip Sunnemar <filipsunnemar@hotmail.com>
2020-09-11 15:54:55 +0200

    Updates

---
+++ precache-manifest.81e330d41e2a540e8a6bf66d3332d4bc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "706cb0389565bd8bd495857824c522c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d6a8d41dad68472ed05",
+    "url": "/static/css/main.0fc6ebf3.chunk.css"
+  },
+  {
+    "revision": "87ed9eec842a77c25079",
+    "url": "/static/js/2.90528f1b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.90528f1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d6a8d41dad68472ed05",
+    "url": "/static/js/main.cb7a3d19.chunk.js"
+  },
+  {
+    "revision": "489424f09525a28448ea",
+    "url": "/static/js/runtime-main.dfd851ae.js"
+  },
+  {
+    "revision": "6614d5b4a37be6a7a043735aa02ce61a",
+    "url": "/static/media/Ark_logo.6614d5b4.png"
+  },
+  {
+    "revision": "736ae413d0657e387323f86f2937fc75",
+    "url": "/static/media/Lego_ship.736ae413.jpg"
+  },
+  {
+    "revision": "a11ed456d5d848ee69fc50844669b2f7",
+    "url": "/static/media/Monster_truck.a11ed456.jpg"
+  }
+]);
